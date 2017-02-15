5deb745022b20c146f6ac739597216075f314f9a
Ayodeji Remi-Omosowon <remiomosowon@gmail.com>
2017-02-15 11:36:07 +0000

    Add simple game init test with not enough players

--- src/tests/unit_tests/test_game.py
+++ src/tests/unit_tests/test_game.py
@@ -19,6 +19,9 @@ class Game(object):
         self.rounds = 0
 
     def initialise(self, num_players, num_rounds):
+        if num_players < 2:
+            raise Exception
+
         for x in range(num_players):
             self.players.append(Player())
 
@@ -38,3 +41,10 @@ class GameTest(unittest.TestCase):
         game.initialise(num_players=2, num_rounds=1)
         self.assertEquals(len(game.players), 2)
         self.assertEquals(game.rounds, 1)
+
+    def test_initialise_simple_game_without_enough_players(self):
+        game = Game()
+        with self.assertRaises(Exception):
+            game.initialise(num_players=1, num_rounds=1)
+
+
